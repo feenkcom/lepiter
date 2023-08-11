427a83b3d1bf615d809a04777e13a841623c3b22
Juraj Kubelka <juraj.kubelka.yuyv8@slmail.me>
2023-08-11 19:55:38 -0400

    make `gtMethod` link expandable [feenkcom/gtoolkit#3388]

--- doc/gtbook/assets/v1/src/js/lepiter-snippets.js
+++ doc/gtbook/assets/v1/src/js/lepiter-snippets.js
@@ -1,16 +1,16 @@
 function toggleExpandableAnnotation(event) {
     let $element = $(event.currentTarget);
-    let $sibling = $element.next('.expandable_annotation_body');
-    if ($sibling.hasClass('hidden_annotation_v3')) {
-        $sibling.removeClass("hidden_annotation_v3").addClass("visible_annotation_v3");
-        $element.removeClass("collapsed_annotation_body").addClass("expanded_annotation_body");
+    let $sibling = $element.next('.expandable-annotation-view');
+    if ($sibling.hasClass('collapsed-annotation-view')) {
+        $sibling.removeClass("collapsed-annotation-view").addClass("expanded-annotation-view");
+        $element.removeClass("annotation-sibling-is-collapsed").addClass("annotation-sibling-is-expanded");
     } else {
-        $sibling.removeClass("visible_annotation_v3").addClass("hidden_annotation_v3");
-        $element.removeClass("expanded_annotation_body").addClass("collapsed_annotation_body");
+        $sibling.removeClass("expanded-annotation-view").addClass("collapsed-annotation-view");
+        $element.removeClass("annotation-sibling-is-expanded").addClass("annotation-sibling-is-collapsed");
     }
 }
 function addExpandableAnnotations() {
-    $(".expandable_annotation_label")
+    $(".expandable-annotation-label")
         .click(toggleExpandableAnnotation);
 }
 
